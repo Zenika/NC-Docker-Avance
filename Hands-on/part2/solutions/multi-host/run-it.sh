e372a8e092ce5094e806e57e2cf48a0f613b182b
Vincent Demeester <vincent@sbr.pm>
2015-03-05 16:51:33 +0100

    Update multi-host to kinda work

--- Hands-on/part2/solutions/multi-host/run-it.sh
+++ Hands-on/part2/solutions/multi-host/run-it.sh
@@ -12,36 +12,50 @@ notice() {
 # Exit on error
 set -e
 
-. ../../../source-me
+sudo modprobe vboxdrv
+sudo modprobe vboxnetflt
+sudo modprobe vboxnetadp
+sudo modprobe vboxpci
+
+HANDS_ON=../../..
+IMAGES=${HANDS_ON}/images/
+. ${HANDS_ON}/source-me
 
 create_machine() {
     NODE_NAME=$1
     shift
     SWARM_CLUSTER_ID=$1
     docker-machine ls | grep ${NODE_NAME} || {
-        docker-machine create -d virtualbox --virtualbox-disk-size '6000' --virtualbox-memory '512' --swarm --swarm-discovery=token://${SWARM_CLUSTER_ID} ${NODE_NAME}
+        docker-machine -D create -d virtualbox --virtualbox-disk-size '6000' --virtualbox-memory '512' --swarm --swarm-discovery=token://${SWARM_CLUSTER_ID} ${NODE_NAME}
         $(docker-machine env ${NODE_NAME})
-        test -e postgres.tar.xz && docker load --input postgres.tar.xz || docker pull postgres
-        test -e redis.tar && docker load --input redis.tar.xz || docker pull redis
-        test -e zenika_nc-avance-solution.tar.xz && docker load import --input zenika_nc-avance-solution.tar.xz || docker pull zenika/nc-avance-solution
+        echo "Wait 5s to let docker engine start.."
+        sleep 5s
+        echo $IMAGES
+        ls $IMAGES/
+        read
+        test -e ${IMAGES}/postgres.tar.xz && docker load --input ${IMAGES}/postgres.tar.xz || docker pull postgres
+        test -e ${IMAGES}/redis.tar.xz && docker load --input ${IMAGES}/redis.tar.xz || docker pull redis
+        test -e ${IMAGES}/zenika_nc-avance-solution.tar.xz && docker load --input ${IMAGES}/zenika_nc-avance-solution.tar.xz || docker pull zenika/nc-avance-solution
     }
 }
 
-docker-machine ls | grep swarm-master || {
+docker-machine ls | grep swarm-manager || {
     log "Create the swarm cluster"
     CLUSTER_ID=$(swarm create)
     notice "cluster id : ${CLUSTER_ID}"
-    docker-machine create -d virtualbox --virtualbox-disk-size '3000' --virtualbox-memory '256' --swarm --swarm-master --swarm-discovery=token://${CLUSTER_ID} swarm-master
+    docker-machine -D create -d virtualbox --virtualbox-disk-size '3000' --virtualbox-memory '256' --swarm --swarm-master --swarm-discovery=token://${CLUSTER_ID} swarm-manager
 }
+
+read
 
 log "Create the node machines"
 create_machine node1 ${CLUSTER_ID}
 create_machine node2 ${CLUSTER_ID}
-create_machine node3 ${CLUSTER_ID}
-create_machine node4 ${CLUSTER_ID}
+#create_machine node3 ${CLUSTER_ID}
+#create_machine node4 ${CLUSTER_ID}
 
 log "Setup environment variables"
-$(docker-machine env --swarm swarm-master)
+$(docker-machine env --swarm swarm-manager)
 
 log "Clone the sources"
 test -d flask-by-example || git clone https://github.com/vdemeester/flask-by-example.git && notice "already done"
