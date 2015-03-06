8df01cd85826eb24cfbbf52ae1af8a601fc822be
Vincent Demeester <vincent+github@demeester.fr>
2015-03-06 14:29:56 +0100

    Set a different name for the "formation"

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,11 @@
 module.exports = function(grunt) {
-
+    grunt.initConfig({
+        zenika: {
+            formation: {
+                name: 'Nightclazz Docker Avancé'
+            }
+        }
+    });
     grunt.loadTasks('node_modules/zenika-formation-framework');
 	
     grunt.registerTask('default', ['displaySlides']);
